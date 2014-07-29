d045a2d9c3ac6dbdb73886a82841309b38c67ecd
marroq <dffigueroa@galileo.edu>
2014-07-29 10:12:18 -0600

    Tarea #8, Pequeña configuración en Compiler.java y Configuration.java

--- cc4/compiler/lib/Configuration.java
+++ cc4/compiler/lib/Configuration.java
@@ -5,9 +5,4 @@ public class Configuration {
 
 	public static Hashtable flags;
 	public static int stopStage;
-
-	public Configuration(Hashtable < String, String > flags, int stopStage) {
-		this.flags=flags;
-		this.stopStage=stopStage;
-	}
 }

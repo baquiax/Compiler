0cf00537b30f235474ec4780188d1bb46f4d2d2e
dfer <dfer@dfer.(none)>
2014-09-22 02:08:37 -0600

    Modificaciones, Decaf.g agregado por razones de no arruinar el parser, se trabajara con el mismo lexer y mismo parser pero desde archivos generados en el paquete ast sino fallará el parser

--- cc4/compiler/ast/AstVisitor.java
+++ cc4/compiler/ast/AstVisitor.java
@@ -1,11 +1,11 @@
 package compiler.ast;
 import compiler.parser.ParserDecaf;
-import compiler.parser.ParserDecafVistor;
+import compiler.parser.ParserDecafVisitor;
 
 public class AstVisitor extends ParserDecafVisitor<Node> {
 
 	@Override
 	public Node visitProgram(ParserDecaf.ProgramContext ctx) {
-
+		return null;
 	}
 }
--- cc4/compiler/ast/Node.java
+++ cc4/compiler/ast/Node.java
@@ -2,4 +2,4 @@ package compiler.ast;
 
 public abstract class Node {
     public abstract void print(String padding);
-} 
+}
